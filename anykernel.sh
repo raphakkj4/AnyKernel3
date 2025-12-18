95e0a5820e81f0e5a9997e45db49240c8bdb09d2
raphakk <raphaelfox583@gmail.com>
2025-12-17 22:31:36 -0300

    andorid 18 KKKKKKKKKKK

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Hawkeye Ω Kernel by @raphakk
+kernel.string=Kraken Ω Kernel by @raphakk
 do.devicecheck=1
 do.modules=1
 do.systemless=1
@@ -12,7 +12,7 @@ do.cleanup=1
 do.cleanuponabort=0
 device.name1=redwood
 device.name2=redwoodin
-supported.versions=11.0-15.0
+supported.versions=11.0-18.0
 supported.patchlevels=
 supported.vendorpatchlevels=
 '; } # end properties
